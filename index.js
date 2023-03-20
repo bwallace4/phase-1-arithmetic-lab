650e47d3bcfefcf8b85edac725afbf1370b11256
Brandon Wallace <brandonwallace403@gmail.com>
2023-03-20 04:19:11 -0400

    Adds complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor((Math.random() * 100) + 1);
+const num3 = 44;
+const num4 = 10;
+let mod = num3 % num4;
+let max = Math.max(1, 2, 3, 4, 20);
